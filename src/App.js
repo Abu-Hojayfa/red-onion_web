8f2db1141988d03a1f29be1f547b33c576521da7
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-09-12 16:57:08 +0600

    first upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  // const [info, setInfo] = useState({});
+
+  // const formInfo = e =>{
+  //   const data = {...info};
+  //   data[e.target.name] = e.target.value;
+  //   setInfo(data);
+  // };
+
+  // const insertToDb = () =>{
+  //   console.log(info)
+  //   fetch('http://localhost:9000/lunch',{
+  //     method: 'POST',
+  //     headers: {'Content-Type': 'application/json'},
+  //     body: JSON.stringify(info)
+  //   })
+  //   .then((res) => res.json())
+  //   .then(data => console.log(data));
+  // };
+
+  return <div className="App"></div>;
+}
+
+export default App;
