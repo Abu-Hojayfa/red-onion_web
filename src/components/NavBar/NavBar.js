bb3800b3306e90a1a5aad7b06a7c32732cc5e676
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-09-13 19:19:11 +0600

    navbar done

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Logo from "../../assets/images/logo2.png";
 
 const NavBar = () => {
   return (
-    <div>
-      hi
+    <div className="p-4 md:px-28 bg-torch-red-50">
+      <div className="flex justify-between">
+        <img className="w-28 md:w-40 " src={Logo} alt="Logo Of Red Onion" />
+        <div className="flex  items-center text-sm  md:text-xl font-semibold ">
+          <p className="cursor-pointer mr-2 text-torch-red-500">
+            <FontAwesomeIcon
+              className="text-black"
+              icon={faShoppingCart}
+            />{" "}
+            (1)
+          </p>
+          <p className="ml-2 mr-2 cursor-pointer md:ml-8">Home</p>
+          <button className="ml-2 md:ml-8 py-1 md:py-2 md:pb-3 px-4 md:px-10 rounded-3xl shadow-md text-white bg-torch-red-500 hover:bg-torch-red-600 ">LogIn</button>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "torch-red": {
+          DEFAULT: "#F91944",
+          50: "#FFF9FA",
+          100: "#FEE0E6",
+          200: "#FDAEBD",
+          300: "#FC7C95",
+          400: "#FA4B6C",
+          500: "#F91944",
+          600: "#D9062E",
+          700: "#A80424",
+          800: "#760319",
+          900: "#44020F",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
