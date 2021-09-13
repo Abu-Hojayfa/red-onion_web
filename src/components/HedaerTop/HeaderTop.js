1fcfe17072aecc5239712457303b41da31910650
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-09-13 22:58:51 +0600

    fix design for header

--- src/components/HedaerTop/HeaderTop.js
+++ src/components/HedaerTop/HeaderTop.js
@@ -18,12 +18,12 @@ const HeaderTop = () => {
           <div className="relative w-3/4 md:w-2/5 text-gray-600 focus-within:text-gray-400 mt-4 md:mt-12">
             <input
               type="search"
-              className="pl-3 p-2 md:p-3.5 text-base md:text-xl w-full text-white bg-white rounded-3xl focus:outline-none focus:bg-white focus:text-gray-900"
+              className=" p-2 md:p-3.5 pl-4 md:pl-8 text-base md:text-xl w-full text-white bg-white rounded-full focus:outline-none focus:bg-white focus:text-gray-900 "
               placeholder="Search Food Items"
               autoComplete="off"
             />
             <span className="absolute inset-y-0 right-0 flex items-center pl-2">
-              <button className="ml-2 md:ml-8 py-1.5 md:py-3.5 font-semibold md:text-xl px-4 md:px-12 rounded-3xl shadow-md text-white bg-torch-red-500 hover:bg-torch-red-600 ">
+              <button className="ml-2 md:ml-8 py-1.5 md:py-3.5 font-semibold md:text-xl px-4 md:px-12 rounded-full shadow-md text-white bg-torch-red-500 hover:bg-torch-red-600 ">
                 Search
               </button>
             </span>
