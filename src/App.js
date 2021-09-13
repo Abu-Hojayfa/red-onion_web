cf22b6d47ce3d015c5293fd7411f169e99aebf45
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-09-13 13:06:27 +0600

    add images

--- src/App.js
+++ src/App.js
@@ -2,26 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  // const [info, setInfo] = useState({});
-
-  // const formInfo = e =>{
-  //   const data = {...info};
-  //   data[e.target.name] = e.target.value;
-  //   setInfo(data);
-  // };
-
-  // const insertToDb = () =>{
-  //   console.log(info)
-  //   fetch('http://localhost:9000/lunch',{
-  //     method: 'POST',
-  //     headers: {'Content-Type': 'application/json'},
-  //     body: JSON.stringify(info)
-  //   })
-  //   .then((res) => res.json())
-  //   .then(data => console.log(data));
-  // };
-
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
 }
 
 export default App;
