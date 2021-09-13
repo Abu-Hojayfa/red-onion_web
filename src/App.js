3948190e10119df27f5763c72b11827f8ec0ba10
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-09-13 18:05:06 +0600

    intialize tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Home from "./screens/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Home />
     </div>
   );
 }
