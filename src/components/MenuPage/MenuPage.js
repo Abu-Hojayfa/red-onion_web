7f8d4b834b724017a8add224ecdb8d0a1c2ca752
Abu-Hojayfa <abu.hojayfa100@gmail.com>
2021-09-15 21:02:29 +0600

    loader screen

--- src/components/MenuPage/MenuPage.js
+++ src/components/MenuPage/MenuPage.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import LoaderGif from "../../assets/images/loading.gif";
 
 const MenuPage = () => {
   const [stateOfProduct, setStateOfProduct] = useState("lunch");
   const [foodData, setFoodData] = useState([]);
+  const [loader, setLoader] = useState(true);
 
   const styleOfIndicator =
     "mx-4 md:mx-10 text-lg font-semibold md:text-2xl cursor-pointer border-b-2 border-transparent  px-2 ";
@@ -10,16 +12,19 @@ const MenuPage = () => {
 
   const onClickHandler = (e) => {
     setStateOfProduct(e);
+    setLoader(false);
   };
 
   useEffect(() => {
     fetch(`http://localhost:5000/${stateOfProduct}`)
       .then((res) => res.json())
-      .then((data) => setFoodData(data));
+      .then((data) => {
+        setFoodData(data);
+        setLoader(false);
+      });
   }, [stateOfProduct]);
 
-
-  const checkoutBtnStyleInActive ='bg-gray-300  text-white cursor-not-allowed';
+  const checkoutBtnStyleInActive = "bg-gray-300  text-white cursor-not-allowed";
 
   return (
     <div className="pt-6 md:mt-10">
@@ -50,17 +55,34 @@ const MenuPage = () => {
         </p>
       </div>
 
-      <div className="mt-10 mb-10 w-5/6 md:w-3/4 mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
-        {foodData && foodData.map((data, index) => 
-            <div className="mt-2 shadow-lg md:shadow-none md:hover:shadow-lg py-4 rounded-xl cursor-pointer" key={data.id}>
-              <img className="w-44 md:w-72 mx-auto" src={data.imgURL} alt="Food_Image" />
-              <p className="text-xl pt-4 font-semibold">{data.foodName}</p>
-              <p className="text-base w-2/3 mx-auto text-gray-500 pt-2 font-semibold">{data.shrtDis}</p>
-              <p className="text-2xl py-4 font-bold">${data.price}</p>
-            </div>
-        )}
-      </div>
-      <button className={`py-3 px-10 mb-10  font-semibold text-lg rounded-lg ${checkoutBtnStyleInActive}`}>
+      {loader ? (
+        <img className=" w-56 md:w-96 mx-auto" src={LoaderGif} alt="loading_animation" />
+      ) : (
+        <div className="mt-10 mb-10 w-5/6 md:w-3/4 mx-auto grid grid-cols-1 md:grid-cols-3 gap-4">
+          {foodData &&
+            foodData.map((data, index) => (
+              <div
+                className="mt-2 shadow-lg md:shadow-none md:hover:shadow-lg py-4 rounded-xl cursor-pointer"
+                key={data.id}
+              >
+                <img
+                  className="w-44 md:w-72 mx-auto"
+                  src={data.imgURL}
+                  alt="Food_Image"
+                />
+                <p className="text-xl pt-4 font-semibold">{data.foodName}</p>
+                <p className="text-base w-2/3 mx-auto text-gray-500 pt-2 font-semibold">
+                  {data.shrtDis}
+                </p>
+                <p className="text-2xl py-4 font-bold">${data.price}</p>
+              </div>
+            ))}
+        </div>
+      )}
+
+      <button
+        className={`py-3 px-10 mb-10  font-semibold text-lg rounded-lg ${checkoutBtnStyleInActive}`}
+      >
         Checkout Your Food
       </button>
     </div>
